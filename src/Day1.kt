d3550f3ffcb8876a0482cc869634584869f3ea23
Julian Wiersbitzki <julian@wiersbitzki.com>
2025-12-05 11:21:53 +0100

    day1 second part

--- src/Day1.kt
+++ src/Day1.kt
@@ -4499,24 +4499,34 @@ R25"""
 
 fun main() {
     var position = 50
-    var zero = 0
+    var zeros = 0
     println("The dial starts by pointing at $position")
     input.lines().forEach { line ->
         run {
-            val direction = line.substring(0, 1)
-            var newPosition = when (direction) {
-                "L" -> position - (line.substring(1).toInt() % 100)
-                "R" -> position + (line.substring(1).toInt() % 100)
-                else -> {
-                    throw error("Unexpected direction $direction")
-                }
+            val direction = Direction.valueOf(line.take(1))
+            val clicks = line.substring(1).toInt()
+            repeat(clicks) {
+                position = position.click(direction)
+                if (position.zero) zeros++
             }
-            if (newPosition < 0) newPosition += 100
-            if (newPosition > 99) newPosition -= 100
-            println("The dial is rotated $line to point at $newPosition.")
-            if (newPosition == 0) zero++
-            position = newPosition
+            println("The dial is rotated $line to point at $position.")
         }
     }
-    println("Zeros: $zero")
+    println("Zeros: $zeros")
+}
+
+private fun Int.click(direction: Direction) = direction.click(this)
+
+private val Int.zero
+    get() = this % 100 == 0
+
+enum class Direction {
+    L {
+        override fun click(position: Int) = if (position != 0) position.dec() else 99
+    },
+    R {
+        override fun click(position: Int) = if (position != 99) position.inc() else 0
+    };
+
+    abstract fun click(position: Int): Int
 }
